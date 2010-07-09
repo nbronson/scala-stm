2337c01b27eddc9e78ad014ed6e94e9702d4da65
Nathan Grasso Bronson <nbronson@cs.stanford.edu>
2010-07-09 11:58:57 -0700

    Added object Txn, with methods to perform a dynamic lookup of the current Txn.

--- src/main/scala/scala/concurrent/stmA/Txn.scala
+++ src/main/scala/scala/concurrent/stmA/Txn.scala
@@ -2,4 +2,17 @@
 
 package scala.concurrent.stmA
 
+object Txn {
+  import impl.TxnFactory
+
+  /** Returns `Some(t)` if called from inside the static or dynamic scope of
+   *  the transaction `t`, `None` otherwise.  If an implicit `Txn` is
+   *  available it is used, otherwise a dynamic lookup is performed.
+   */
+  def current(implicit mt: MaybeTxn): Option[Txn] = Option(currentOrNull)
+
+  /** Equivalent to `current getOrElse null`. */
+  def currentOrNull(implicit mt: MaybeTxn): Txn = TxnFactory.instance.currentOrNull
+}
+
 trait Txn extends MaybeTxn
--- src/main/scala/scala/concurrent/stmA/impl/TxnFactory.scala
+++ src/main/scala/scala/concurrent/stmA/impl/TxnFactory.scala
@@ -8,6 +8,26 @@ object TxnFactory {
 }
 
 trait TxnFactory {
-  def currentOrNull(implicit mt: MaybeTxn): Txn
+
+  // If an STM implementation can locate a dynamically scoped Txn directly,
+  // then it should override currentOrNull.  If the dynamic lookup is slower
+  // than an instanceof check, it should override only dynCurrentOrNull.
+
+  /** Returns the `Txn` active on the current thread, or null if none, possibly
+   *  using the statically-bound `MaybeTxn` to reduce the amount of work
+   *  required. 
+   */
+  def currentOrNull(implicit mt: MaybeTxn): Txn = mt match {
+    case t: Txn => t
+    case _ => dynCurrentOrNull
+  }
+
+  /** Returns the `Txn` active on the current thread, or null if none, always
+   *  performing a full dynamic lookup.
+   */
+  def dynCurrentOrNull: Txn
+
+  
+  /** Executes `block` in a transaction. */ 
   def atomic[Z](block: Txn => Z): Z
 }
