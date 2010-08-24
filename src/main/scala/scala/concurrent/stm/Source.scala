70f730a8be1891bc405059c00c24a135e7fd731c
Nathan Grasso Bronson <nbronson@cs.stanford.edu>
2010-08-23 18:13:51 -0700

    Experimental idea for providing the same functionality as CCSTM's ReadResource-s but allowing for a trivial compliant implementation (with simpler semantics to boot).

--- src/main/scala/scala/concurrent/stm/Source.scala
+++ src/main/scala/scala/concurrent/stm/Source.scala
@@ -10,6 +10,12 @@ object Source {
     def apply(): A = get
 
     def get: A
+
+    def getWith[Z](f: A => Z): Z
+
+    def relaxedGet(revalidator: (A, A) => Boolean): A
+
+    def relaxedGetWith[Z](f: A => Z, revalidator: (Z, Z) => Boolean)(implicit txn: Txn): Z
   }
 }
 
@@ -20,4 +26,10 @@ trait Source[+A] {
   def apply()(implicit txn: Txn): A = get
   
   def get(implicit txn: Txn): A
+
+  def getWith[Z](f: A => Z)(implicit txn: Txn): Z
+
+  def relaxedGet(revalidator: (A, A) => Boolean)(implicit txn: Txn): A
+
+  def relaxedGetWith[Z](f: A => Z, revalidator: (Z, Z) => Boolean)(implicit txn: Txn): Z
 }
