dfaf6316f20e8b53cd6c9603edd1d0a4632835b5
Nathan Grasso Bronson <nbronson@cs.stanford.edu>
2010-08-26 10:21:47 -0700

    Added Ref and Ref.View functions with documentation.

--- src/main/scala/scala/concurrent/stm/Ref.scala
+++ src/main/scala/scala/concurrent/stm/Ref.scala
@@ -11,11 +11,141 @@ object Ref extends RefCompanion {
 
   trait View[A] extends Source.View[A] with Sink.View[A] {
 
+    /** Returns a `Ref` that accesses the same memory location as this view.
+     *  The returned `Ref` might be the original reference that was used to
+     *  construct this view, or it might be a `Ref` that is equivalent (and
+     *  `==`) to the original.
+     *  @return a `Ref` that accesses the same memory location as this view.
+     */
     override def unbind: Ref[A]
 
+    /** Works like `set(v)`, but returns the old value.  This is an
+     *  atomic swap, equivalent to atomically performing a `get`
+     *  followed by `set(v)`.
+     *  @return the previous value of the viewed `Ref`.
+     */
+    def swap(v: A): A
+
+    /** Equivalent to atomically executing
+     *  `(if (before == get) { set(after); true } else false)`, but may be more
+     *  efficient, especially if there is no enclosing atomic block.
+     *  @param before a value to compare against the current `Ref` contents.
+     *  @param after a value to store in the `Ref` if `before` was equal to the
+     *      previous cell contents.
+     *  @return true if `before` was equal to the previous value of the bound
+     *      `Ref`, false otherwise.
+     */
+    def compareAndSet(before: A, after: A): Boolean
+
+    /** Equivalent to atomically executing
+     *  `(if (before eq get) { set(after); true } else false)`, but may be more
+     *  efficient, especially if there is no enclosing atomic block.
+     *  @param before a reference whose identity will be compared against the
+     *      current `Ref` contents.
+     *  @param after a value to store in the `Ref` if `before` has the same
+     *      reference identity as the previous cell contents.
+     *  @return true if `before` has the same reference identity as the
+     *      previous value of the bound `Ref`, false otherwise.
+     */
+    def compareAndSetIdentity[B <: A with AnyRef](before: B, after: A): Boolean
+
+    /** Atomically replaces the value ''v'' stored in the `Ref` with
+     *  `f`(''v'').  Some `Ref` implementations may defer execution of `f` or
+     *  call `f` multiple times to avoid transaction conflicts.
+     *  @param f a function that is safe to call multiple times, and safe to
+     *      call later during the enclosing atomic block, if any.
+     */
     def transform(f: A => A)
+
+    /** Atomically replaces the value ''v'' stored in the `Ref` with
+     *  `f`(''v''), returning the old value.  `transform` should be preferred
+     *  if the return value is not needed, since it gives the STM more
+     *  flexibility to avoid transaction conflicts.
+     *  @param f a function that is safe to call multiple times, and safe to
+     *      call later during the enclosing atomic block, if any.
+     *  @return the previous value of the viewed `Ref`.
+     */
+    def getAndTransform(f: A => A): A
+
+    /** Either atomically transforms this reference without blocking and
+     *  returns true, or returns false.  `transform` is to `tryTransform` as
+     *  `set` is to `trySet`.  If this method is called inside an atomic block
+     *  a true return value does not necessarily mean that `f` has already been
+     *  called, just that the transformation will definitely be performed in
+     *  the outer transaction if it commits.
+     *  @param f a function that is safe to call multiple times, and safe to
+     *      call later during the enclosing atomic block, if any.
+     *  @return true if the function was (or will be) applied, false if it was
+     *      not.
+     */
+    def tryTransform(f: A => A): Boolean
+
+    /** Atomically replaces the value ''v'' stored in the `Ref` with
+     *  `pf`(''v'') if `pf.isDefinedAt`(''v''), returning true, otherwise
+     *  leaves the element unchanged and returns false.  `pf.apply` and
+     *  `pf.isDefinedAt` might be invoked multiple times by the STM, and might
+     *  be called later in the enclosing atomic block, if any.
+     *  @param pf a partial function that is safe to call multiple times, and
+     *      safe to call later in the enclosing atomic block, if any.
+     *  @return `pf.isDefinedAt``(''v''), where ''v'' is the element held by
+     *      this `Ref` on entry.
+     */
+    def transformIfDefined(pf: PartialFunction[A,A]): Boolean
+
+    /** Transforms the value stored in the `Ref` by incrementing it.
+     *
+     *  '''Note: Some `Ref` implementations may choose to ignore the passed-in
+     *  `Numeric[A]` instance if `A` is a primitive type.'''
+     *
+     *  @param rhs the quantity by which to increment the value of `unbind`.
+     */
+    def += (rhs: A)(implicit num: Numeric[A]) { transform { v => num.plus(v, rhs) } }
+
+    /** Transforms the value stored in the `Ref` by decrementing it.
+     *
+     *  '''Note: Some `Ref` implementations may choose to ignore the passed-in
+     *  `Numeric[A]` instance if `A` is a primitive type.'''
+     *
+     *  @param rhs the quantity by which to decrement the value of `unbind`.
+     */
+    def -= (rhs: A)(implicit num: Numeric[A]) { transform { v => num.minus(v, rhs) } }
+
+    /** Transforms the value stored in the `Ref` by multiplying it.
+     *
+     *  '''Note: Some `Ref` implementations may choose to ignore the passed-in
+     *  `Numeric[A]` instance if `A` is a primitive type.'''
+     *
+     *  @param rhs the quantity by which to multiple the value of `unbind`.
+     */
+    def *= (rhs: A)(implicit num: Numeric[A]) { transform { v => num.times(v, rhs) } }
+
+    /** Transforms the value stored in the `Ref` by performing a division on
+     *  it, throwing away the remainder if division is not exact for instances
+     *  of `A`.  The careful reader will note that division is actually
+     *  provided by either an implicit `Fractional[A]` or an implicit
+     *  `Integral[A]`, it is not defined on `Numeric[A]`.  Due to problems
+     *  overloading based on the available implicits this method accepts any
+     *  `Numeric[A]` and assumes that it can be converted at runtime into
+     *  exactly one of the two previous types.
+     *
+     *  '''Note: Some `Ref` implementations may choose to ignore the passed-in
+     *  `Integral[A]` instance if `A` is a primitive type.'''
+     *
+     *  @param rhs the quantity by which to divide the value of `unbind`.
+     */
+    def /= (rhs: A)(implicit num: Numeric[A]) {
+      num match {
+        case numF: Fractional[A] => transform { v => numF.div(v, rhs) }
+        case numI: Integral[A] => transform { v => numI.quot(v, rhs) }
+      }
+    }
   }
 }
+
+// All of object Ref's functionality is actually in RefCompanion.  The split
+// allows RefCompanion to be tested independently of the globally configured
+// RefFactory, without introducing an extra level of mutable indirection for
+// normal uses of the companion object.
 
 trait RefCompanion {
   
@@ -90,5 +220,81 @@ trait Ref[A] extends Source[A] with Sink[A] {
   // write-only operations (contravariant) are in Sink
   // read+write operations go here 
 
+  /** Works like `set(v)`, but returns the old value.
+   *  @return the previous value of this `Ref`, as observed by `txn`.
+   *  @throws IllegalStateException if `txn` is not active.
+   */
+  def swap(v: A)(implicit txn: Txn): A
+
+  /** Transforms the value referenced by this `Ref` by applying the function
+   *  `f`.  Acts like `ref.set(f(ref.get))`, but the execution of `f` may be
+   *  deferred or repeated by the STM to reduce transaction conflicts.
+   *  @param f a function that is safe to call multiple times, and safe to
+   *      call later during the transaction.
+   *  @throws IllegalStateException if `txn` is not active.
+   */
   def transform(f: A => A)(implicit txn: Txn)
+
+  /** Transforms the value ''v'' referenced by this `Ref` by to
+   *  `pf.apply`(''v''), but only if `pf.isDefinedAt`(''v'').  Returns true if
+   *  a transformation was performed, false otherwise.  `pf.apply` and
+   *  `pf.isDefinedAt` may be deferred or repeated by the STM to reduce
+   *  transaction conflicts.
+   *  @param pf a partial function that is safe to call multiple times, and
+   *      safe to call later in the transaction.
+   *  @return `pf.isDefinedAt(v)`, where `v` was the value of this `Ref`
+   *      before transformation (if any).
+   *  @throws IllegalStateException if `txn` is not active.
+   */
+  def transformIfDefined(pf: PartialFunction[A,A])(implicit txn: Txn): Boolean
+
+  /** Transforms the value stored in the `Ref` by incrementing it.
+   *
+   *  '''Note: Some `Ref` implementations may choose to ignore the passed-in
+   *  `Numeric[A]` instance if `A` is a primitive type.'''
+   *
+   *  @param rhs the quantity by which to increment the value of this `Ref`.
+   *  @throws IllegalStateException if `txn` is not active. */
+  def += (rhs: A)(implicit txn: Txn, num: Numeric[A]) { transform { v => num.plus(v, rhs) } }
+
+  /** Transforms the value stored in the `Ref` by decrementing it.
+   *
+   *  '''Note: Some `Ref` implementations may choose to ignore the passed-in
+   *  `Numeric[A]` instance if `A` is a primitive type.'''
+   *
+   *  @param rhs the quantity by which to decrement the value of this `Ref`.
+   *  @throws IllegalStateException if `txn` is not active. */
+  def -= (rhs: A)(implicit txn: Txn, num: Numeric[A]) { transform { v => num.minus(v, rhs) } }
+
+  /** Transforms the value stored in the `Ref` by multiplying it.
+   *
+   *  '''Note: Some `Ref` implementations may choose to ignore the passed-in
+   *  `Numeric[A]` instance if `A` is a primitive type.'''
+   *
+   *  @param rhs the quantity by which to multiply the value of this `Ref`.
+   *  @throws IllegalStateException if `txn` is not active.
+   */
+  def *= (rhs: A)(implicit txn: Txn, num: Numeric[A]) { transform { v => num.times(v, rhs) } }
+
+  /** Transforms the value stored in the `Ref` by performing a division on
+   *  it, throwing away the remainder if division is not exact for instances
+   *  of `A`.  The careful reader will note that division is actually
+   *  provided by either an implicit `Fractional[A]` or an implicit
+   *  `Integral[A]`, it is not defined on `Numeric[A]`.  Due to problems
+   *  overloading based on the available implicits this method accepts any
+   *  `Numeric[A]` and assumes that it can be converted at runtime into
+   *  exactly one of the two previous types.
+   *
+   *  '''Note: Some `Ref` implementations may choose to ignore the passed-in
+   *  `Integral[A]` instance if `A` is a primitive type.'''
+   *
+   *  @param rhs the quantity by which to divide the value of this `Ref`.
+   *  @throws IllegalStateException if `txn` is not active.
+   */
+  def /= (rhs: A)(implicit txn: Txn, num: Numeric[A]) {
+    num match {
+      case numF: Fractional[A] => transform { v => numF.div(v, rhs) }
+      case numI: Integral[A] => transform { v => numI.quot(v, rhs) }
+    }
+  }
 }
