de92961f69d6333a767a07e4e3a2f353414aaaf6
Nathan Grasso Bronson <nbronson@cs.stanford.edu>
2010-08-27 16:35:08 -0700

    Added some tests that won't pass until there is actually an implementation.

--- src/test/scala/scala/concurrent/stm/MaybeTxnSuite.scala
+++ src/test/scala/scala/concurrent/stm/MaybeTxnSuite.scala
@@ -14,4 +14,75 @@ class MaybeTxnSuite extends FunSuite {
   test("implicit TxnUnknown match") {
     assert(implicitly[MaybeTxn] eq TxnUnknown)
   }
+
+  test("TxnUnknown is found") {
+    assert(context eq TxnUnknown)
+  }
+
+  test("Txn is found") {
+    atomic { t0 =>
+      implicit val t = t0
+      assert(context eq t)
+    }
+  }
+
+  private def context(implicit mt: MaybeTxn) = mt
+
+  test("Static nesting lookup") {
+    val x = Ref(10)
+    atomic { implicit t =>
+      assert(x() === 10)
+      x() = 11
+      atomic { implicit t =>
+        assert(x() === 11)
+        x() = 12
+        atomic { implicit t =>
+          assert(x() === 12)
+          x() = 13
+        }
+        assert(x() === 13)
+      }
+      assert(x() === 13)
+    }
+    assert(x.single() === 13)
+  }
+
+  test("Dynamic nesting lookup") {
+    val x = Ref(10)
+    val xs = x.single
+    def loop(expected: Int) {
+      atomic { implicit t =>
+        assert(x() === expected)
+        assert(xs() === expected)
+        x() = expected + 1
+        if (expected < 100)
+          loop(expected + 1)
+        assert(x() === 101)
+      }
+    }
+    loop(10)
+    assert(xs() === 101)
+    assert(x.single() === 101)
+  }
+
+  test("Static vs dynamic lookup") {
+    implicit var t0: Txn = null
+    atomic { t =>
+      t0 = t
+      assert(Txn.current === Some(t))
+      assert(Txn.currentOrNull eq t)
+      assert(impl.STMImpl.instance.dynCurrentOrNull eq t)
+    }
+    assert(t0.status === Txn.Committed)
+    assert(Txn.current === Some(t0))
+    assert(Txn.currentOrNull eq t0)
+    assert(impl.STMImpl.instance.dynCurrentOrNull eq null)
+    atomic { t =>
+      assert(t0 ne t)
+      assert(t.status === Txn.Active)
+      assert(Txn.current === Some(t0))
+      assert(Txn.currentOrNull eq t0)
+      assert(impl.STMImpl.instance.dynCurrentOrNull eq t)
+    }
+  }
 }
