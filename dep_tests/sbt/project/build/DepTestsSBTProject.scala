8402ec17d74de50dd035e86e0d3b5eb162e3a609
Nathan Grasso Bronson <nbronson@cs.stanford.edu>
2010-12-05 14:30:34 -0800

    Added an sbt hello world project that tests sbt deployment and dependency.

---
+++ dep_tests/sbt/project/build/DepTestsSBTProject.scala
@@ -0,0 +1,8 @@
+import sbt._
+
+class DepTestsSBTProject(info: ProjectInfo) extends DefaultProject(info) {
+  val scala_tools_snapshots = "Scala-Tools Maven2 Repository - snapshots" at "http://scala-tools.org/repo-snapshots"
+  val scala_stm = "org.scala-tools" %% "scala-stm" % "0.1-SNAPSHOT"
+
+  // val scala_stm = "org.scala-tools" %% "scala-stm" % "0.1"
+}
