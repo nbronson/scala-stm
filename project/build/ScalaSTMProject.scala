86d8097685691520d07188318b58f68e6231690c
Nathan Grasso Bronson <nbronson@cs.stanford.edu>
2010-12-05 14:27:08 -0800

    Project file picks correct deployment target using version.

--- project/build/ScalaSTMProject.scala
+++ project/build/ScalaSTMProject.scala
@@ -3,10 +3,21 @@
 import sbt._
 
 class ScalaSTMProject(info: ProjectInfo) extends DefaultProject(info) {
+
+  //// Compilation
+
   val scalatest = "org.scalatest" % "scalatest" % "1.2"
 
+
+  //// Deployment
+
   override def managedStyle = ManagedStyle.Maven
-  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
-  //val publishTo = "Scala Tools Nexus" at //"http://nexus.scala-tools.org/content/repositories/releases/"
+
+  val publishTo = if (version.toString endsWith "-SNAPSHOT") {
+    "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
+  } else {
+    "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+  }
+
   Credentials(Path.userHome / ".ivy2" / ".credentials", log)
 }
