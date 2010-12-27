429b8171f7899fd8acf7b7edb101e48df40579b0
Nathan Grasso Bronson <nbronson@cs.stanford.edu>
2010-12-26 17:18:47 -0800

    Split TxnHashTrie into object and abstract class.  Added contention estimation with probabilistic sampling and smoothing to trigger aggressive splitting of leaves.

--- src/main/scala/scala/concurrent/stm/skel/HashTrieTMap.scala
+++ src/main/scala/scala/concurrent/stm/skel/HashTrieTMap.scala
@@ -3,19 +3,18 @@
 package scala.concurrent.stm
 package skel
 
-import skel.{TxnHashTrie => Impl}
 import scala.collection.mutable.Builder
 
 object HashTrieTMap {
   
-  def empty[A, B]: TMap[A, B] = new HashTrieTMap(Ref(Impl.emptyMapNode[A, B]).single)
+  def empty[A, B]: TMap[A, B] = new HashTrieTMap(Ref(TxnHashTrie.emptyMapNode[A, B]).single)
 
   def newBuilder[A, B] = new Builder[(A, B), TMap[A, B]] {
-    var root = Impl.emptyMapBuildingNode[A, B]
+    var root = TxnHashTrie.emptyMapBuildingNode[A, B]
 
-    def clear() { root = Impl.emptyMapBuildingNode[A, B] }
+    def clear() { root = TxnHashTrie.emptyMapBuildingNode[A, B] }
 
-    def += (kv: (A, B)): this.type = { root = Impl.buildingPut(root, kv._1, kv._2) ; this }
+    def += (kv: (A, B)): this.type = { root = TxnHashTrie.buildingPut(root, kv._1, kv._2) ; this }
 
     def result(): TMap[A, B] = {
       val r = root
@@ -25,47 +24,48 @@ object HashTrieTMap {
   }
 }
 
-private[skel] class HashTrieTMap[A, B] private (private val root: Ref.View[Impl.Node[A, B]]) extends TMapViaClone[A, B] {
+private[skel] class HashTrieTMap[A, B] private (root0: Ref.View[TxnHashTrie.Node[A, B]]
+                                                 ) extends TxnHashTrie[A, B](root0) with TMapViaClone[A, B] {
 
   //// construction
 
-  override def empty: TMap.View[A, B] = new HashTrieTMap(Ref(Impl.emptyMapNode[A, B]).single)
-  override def clone(): HashTrieTMap[A, B] = new HashTrieTMap(Impl.clone(root))
+  override def empty: TMap.View[A, B] = new HashTrieTMap(Ref(TxnHashTrie.emptyMapNode[A, B]).single)
+  override def clone(): HashTrieTMap[A, B] = new HashTrieTMap(cloneRoot)
 
   //// TMap.View aggregates
 
-  override def isEmpty: Boolean = !Impl.sizeGE(root, 1)
-  override def size: Int = Impl.size(root)
-  override def iterator: Iterator[(A, B)] = Impl.mapIterator(root)
-  override def foreach[U](f: ((A, B)) => U) { Impl.mapForeach(root, f) }
+  override def isEmpty: Boolean = !singleSizeGE(1)
+  override def size: Int = singleSize
+  override def iterator: Iterator[(A, B)] = mapIterator
+  override def foreach[U](f: ((A, B)) => U) { singleMapForeach(f) }
 
-  override def clear() { root() = Impl.emptyMapNode[A, B] }
+  override def clear() { root() = TxnHashTrie.emptyMapNode[A, B] }
 
   //// TMap.View per-element
 
-  override def contains(key: A): Boolean = Impl.contains(root, key)
-  override def apply(key: A): B = Impl.getOrThrow(root, key)
-  def get(key: A): Option[B] = Impl.get(root, key)
+  override def contains(key: A): Boolean = singleContains(key)
+  override def apply(key: A): B = singleGetOrThrow(key)
+  def get(key: A): Option[B] = singleGet(key)
 
-  override def put(key: A, value: B): Option[B] = Impl.put(root, key, value)
-  override def update(key: A, value: B) { Impl.put(root, key, value) }
-  override def += (kv: (A, B)): this.type = { Impl.put(root, kv._1, kv._2) ; this }
+  override def put(key: A, value: B): Option[B] = singlePut(key, value)
+  override def update(key: A, value: B) { singlePut(key, value) }
+  override def += (kv: (A, B)): this.type = { singlePut(kv._1, kv._2) ; this }
 
-  override def remove(key: A): Option[B] = Impl.remove(root, key)
-  override def -= (key: A): this.type = { Impl.remove(root, key) ; this }
+  override def remove(key: A): Option[B] = singleRemove(key)
+  override def -= (key: A): this.type = { singleRemove(key) ; this }
 
   //// optimized TMap versions
 
-  override def foreach[U](f: ((A, B)) => U)(implicit txn: InTxn) = Impl.mapForeach(root.ref, f)
+  override def foreach[U](f: ((A, B)) => U)(implicit txn: InTxn) = txnMapForeach(f)
 
-  override def contains(key: A)(implicit txn: InTxn): Boolean = Impl.contains(root.ref, key)
-  override def apply(key: A)(implicit txn: InTxn): B = Impl.getOrThrow(root.ref, key)
-  override def get(key: A)(implicit txn: InTxn): Option[B] = Impl.get(root.ref, key)
+  override def contains(key: A)(implicit txn: InTxn): Boolean = txnContains(key)
+  override def apply(key: A)(implicit txn: InTxn): B = txnGetOrThrow(key)
+  override def get(key: A)(implicit txn: InTxn): Option[B] = txnGet(key)
 
-  override def put(key: A, value: B)(implicit txn: InTxn): Option[B] = Impl.put(root.ref, key, value)
-  override def update(key: A, value: B)(implicit txn: InTxn) { Impl.put(root.ref, key, value) }
-  override def += (kv: (A, B))(implicit txn: InTxn): this.type = { Impl.put(root.ref, kv._1, kv._2) ; this }
+  override def put(key: A, value: B)(implicit txn: InTxn): Option[B] = txnPut(key, value)
+  override def update(key: A, value: B)(implicit txn: InTxn) { txnPut(key, value) }
+  override def += (kv: (A, B))(implicit txn: InTxn): this.type = { txnPut(kv._1, kv._2) ; this }
 
-  override def remove(key: A)(implicit txn: InTxn): Option[B] = Impl.remove(root.ref, key)
-  override def -= (key: A)(implicit txn: InTxn): this.type = { Impl.remove(root.ref, key) ; this }
+  override def remove(key: A)(implicit txn: InTxn): Option[B] = txnRemove(key)
+  override def -= (key: A)(implicit txn: InTxn): this.type = { txnRemove(key) ; this }
 }
--- src/main/scala/scala/concurrent/stm/skel/HashTrieTSet.scala
+++ src/main/scala/scala/concurrent/stm/skel/HashTrieTSet.scala
@@ -3,19 +3,18 @@
 package scala.concurrent.stm
 package skel
 
-import skel.{TxnHashTrie => Impl}
 import scala.collection.mutable.Builder
 
 object HashTrieTSet {
 
-  def empty[A]: TSet[A] = new HashTrieTSet(Ref(Impl.emptySetNode[A]).single)
+  def empty[A]: TSet[A] = new HashTrieTSet(Ref(TxnHashTrie.emptySetNode[A]).single)
 
   def newBuilder[A] = new Builder[A, TSet[A]] {
-    var root = Impl.emptySetBuildingNode[A]
+    var root = TxnHashTrie.emptySetBuildingNode[A]
 
-    def clear() { root = Impl.emptySetBuildingNode[A] }
+    def clear() { root = TxnHashTrie.emptySetBuildingNode[A] }
 
-    def += (elem: A): this.type = { root = Impl.buildingAdd(root, elem) ; this }
+    def += (elem: A): this.type = { root = TxnHashTrie.buildingAdd(root, elem) ; this }
 
     def result(): TSet[A] = {
       val r = root
@@ -25,40 +24,41 @@ object HashTrieTSet {
   }
 }
 
-private[skel] class HashTrieTSet[A] private (private val root: Ref.View[Impl.SetNode[A]]) extends TSetViaClone[A] {
+private[skel] class HashTrieTSet[A] private (root0: Ref.View[TxnHashTrie.SetNode[A]]
+                                              ) extends TxnHashTrie[A, AnyRef](root0) with TSetViaClone[A] {
 
   //// construction
 
-  override def empty: TSet.View[A] = new HashTrieTSet(Ref(Impl.emptySetNode[A]).single)  
-  override def clone(): HashTrieTSet[A] = new HashTrieTSet(Impl.clone(root))
+  override def empty: TSet.View[A] = new HashTrieTSet(Ref(TxnHashTrie.emptySetNode[A]).single)  
+  override def clone(): HashTrieTSet[A] = new HashTrieTSet(cloneRoot)
 
   //// TSet.View aggregates
 
-  override def isEmpty: Boolean = !Impl.sizeGE(root, 1)
-  override def size: Int = Impl.size(root)
-  override def iterator: Iterator[A] = Impl.setIterator(root)
-  override def foreach[U](f: A => U) { Impl.setForeach(root, f) }
-  override def clear() { root() = Impl.emptySetNode[A] }
+  override def isEmpty: Boolean = !singleSizeGE(1)
+  override def size: Int = singleSize
+  override def iterator: Iterator[A] = setIterator
+  override def foreach[U](f: A => U) { singleSetForeach(f) }
+  override def clear() { root() = TxnHashTrie.emptySetNode[A] }
 
   //// TSet.View per-element
 
-  def contains(elem: A): Boolean = Impl.contains(root, elem)
+  def contains(elem: A): Boolean = singleContains(elem)
 
-  override def add(elem: A): Boolean = Impl.put(root, elem, null).isEmpty
-  override def += (elem: A): this.type = { Impl.put(root, elem, null) ; this }
+  override def add(elem: A): Boolean = singlePut(elem, null).isEmpty
+  override def += (elem: A): this.type = { singlePut(elem, null) ; this }
 
-  override def remove(elem: A): Boolean = !Impl.remove(root, elem).isEmpty
-  override def -= (elem: A): this.type = { Impl.remove(root, elem) ; this }
+  override def remove(elem: A): Boolean = !singleRemove(elem).isEmpty
+  override def -= (elem: A): this.type = { singleRemove(elem) ; this }
 
   //// optimized TSet versions
 
-  override def foreach[U](f: A => U)(implicit txn: InTxn) { Impl.setForeach(root.ref, f) }
+  override def foreach[U](f: A => U)(implicit txn: InTxn) { txnSetForeach(f) }
 
-  override def contains(elem: A)(implicit txn: InTxn): Boolean = Impl.contains(root.ref, elem)
+  override def contains(elem: A)(implicit txn: InTxn): Boolean = txnContains(elem)
 
-  override def add(elem: A)(implicit txn: InTxn): Boolean = Impl.put(root.ref, elem, null ).isEmpty
-  override def += (elem: A)(implicit txn: InTxn): this.type = { Impl.put(root.ref, elem, null) ; this }
+  override def add(elem: A)(implicit txn: InTxn): Boolean = txnPut(elem, null ).isEmpty
+  override def += (elem: A)(implicit txn: InTxn): this.type = { txnPut(elem, null) ; this }
   
-  override def remove(elem: A)(implicit txn: InTxn): Boolean = !Impl.remove(root.ref, elem).isEmpty
-  override def -= (elem: A)(implicit txn: InTxn): this.type = { Impl.remove(root.ref, elem) ; this }
+  override def remove(elem: A)(implicit txn: InTxn): Boolean = !txnRemove(elem).isEmpty
+  override def -= (elem: A)(implicit txn: InTxn): this.type = { txnRemove(elem) ; this }
 }
