c6b89276937b5c1dadb55544de01405c75483aba
Nathan Grasso Bronson <nbronson@cs.stanford.edu>
2011-03-11 22:32:42 -0800

    sbt build now supports 2.9.0-SNAPSHOT, although not on by default
    sbt ++2.9.0-SNAPSHOT ... to select

--- project/build/ScalaSTMProject.scala
+++ project/build/ScalaSTMProject.scala
@@ -1,13 +1,19 @@
 /* scala-stm - (c) 2010, Stanford University, PPL */
 
 import sbt._
+import java.io.File
 
 class ScalaSTMProject(info: ProjectInfo) extends DefaultProject(info) {
 
   //// Compilation
 
-  val scalatest = "org.scalatest" % "scalatest" % "1.2"
+  val scala_tools_snapshots = "Scala-Tools Maven2 Repository - snapshots" at "http://scala-tools.org/repo-snapshots"
 
+  val scalatest = if (buildScalaVersion.startsWith("2.9.")) {
+    "org.scalatest" % "scalatest" % "1.4-SNAPSHOT"
+  } else {
+    "org.scalatest" % "scalatest" % "1.3"
+  }
 
   //// Testing
 
