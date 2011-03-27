cfaba2bc19af94bce73d40ca908fdea126c26c81
Nathan Grasso Bronson <nbronson@cs.stanford.edu>
2011-03-26 19:04:19 -0700

    version 0.3 to 1.0-SNAPSHOT

--- dep_tests/sbt/project/build/DepTestsSBTProject.scala
+++ dep_tests/sbt/project/build/DepTestsSBTProject.scala
@@ -1,6 +1,6 @@
 import sbt._
 
 class DepTestsSBTProject(info: ProjectInfo) extends DefaultProject(info) {
-  //val scala_tools_snapshots = "Scala-Tools Maven2 Repository - snapshots" at "http://scala-tools.org/repo-snapshots"
-  val scala_stm = "org.scala-tools" %% "scala-stm" % "0.3"
+  val scala_tools_snapshots = "Scala-Tools Maven2 Repository - snapshots" at "http://scala-tools.org/repo-snapshots"
+  val scala_stm = "org.scala-tools" %% "scala-stm" % "1.0-SNAPSHOT"
 }
