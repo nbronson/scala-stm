dec8d446d8295273b4b8b5289d12bedfd1cd9a3b
Nathan Grasso Bronson <nbronson@cs.stanford.edu>
2011-03-26 18:20:18 -0700

    Filter scalatest from pom.xml

--- project/build/ScalaSTMProject.scala
+++ project/build/ScalaSTMProject.scala
@@ -1,5 +1,6 @@
 /* scala-stm - (c) 2010, Stanford University, PPL */
 
+import scala.xml._
 import sbt._
 import java.io.File
 
@@ -23,6 +24,22 @@ class ScalaSTMProject(info: ProjectInfo) extends DefaultProject(info) {
   //// Deployment
 
   override def managedStyle = ManagedStyle.Maven
+
+  // scalatest isn't needed for the published JAR.  There's probably a cleaner
+  // way to exclude it, but this works
+  override def pomPostProcess(pom: Node): Node = {
+    val dep = (pom \\ "dependency") find { x => (x \ "artifactId") == "scalatest" }
+    val repo = (pom \\ "repository") find { x => (x \ "name").text endsWith "snapshots" }
+    rm(pom, Set.empty[Node] ++ dep ++ repo)
+  }
+
+  private def rm(root: Node, targets: Set[Node]): Node = root match {
+    case x: Elem => {
+      val ch = x.child filter { !targets.contains(_) } map { rm(_, targets) }
+      Elem(x.prefix, x.label, x.attributes, x.scope, ch: _*)
+    }
+    case x => x
+  }
 
   val publishTo = if (version.toString endsWith "-SNAPSHOT") {
     "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
