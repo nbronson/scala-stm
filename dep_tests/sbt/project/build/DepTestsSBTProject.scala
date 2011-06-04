4ba1711d69574fcdce672ec49ee887675e333848
Nathan Grasso Bronson <nbronson@cs.stanford.edu>
2011-06-04 14:56:23 -0700

    reverted devel branch to 0.4-SNAPSHOT
    Some additional API changes should be made based on feedback with Akka
    and Multiverse teams. Lowering the development branch's number to
    reflect the increased distance from a 1.0 final release.

--- dep_tests/sbt/project/build/DepTestsSBTProject.scala
+++ dep_tests/sbt/project/build/DepTestsSBTProject.scala
@@ -2,5 +2,5 @@ import sbt._
 
 class DepTestsSBTProject(info: ProjectInfo) extends DefaultProject(info) {
   val scala_tools_snapshots = "Scala-Tools Maven2 Repository - snapshots" at "http://scala-tools.org/repo-snapshots"
-  val scala_stm = "org.scala-tools" %% "scala-stm" % "1.0-SNAPSHOT"
+  val scala_stm = "org.scala-tools" %% "scala-stm" % "0.4-SNAPSHOT"
 }
