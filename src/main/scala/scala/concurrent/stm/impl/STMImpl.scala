46c70f33c22cefe9f36836d462f799065c65a469
Nathan Grasso Bronson <nbronson@cs.stanford.edu>
2011-08-20 15:13:26 -0700

    scaladoc tweaks

--- src/main/scala/scala/concurrent/stm/impl/STMImpl.scala
+++ src/main/scala/scala/concurrent/stm/impl/STMImpl.scala
@@ -1,4 +1,4 @@
-/* scala-stm - (c) 2009-2010, Stanford University, PPL */
+/* scala-stm - (c) 2009-2011, Stanford University, PPL */
 
 package scala.concurrent.stm
 package impl
@@ -7,6 +7,40 @@ private[impl] object STMImplHolder {
   var instance: STMImpl = STMImpl.createInstance()
 }
 
+/** `STMImpl` gathers all of the functionality required to plug an STM
+ *  implementation into `scala.concurrent.stm`.  Only one implementation can
+ *  be selected, because `Ref`s and atomic blocks from different STM
+ *  implementations are not compatible.  `STMImpl.instance` returns the
+ *  `STMImpl` instance that has been selected for this program execution.
+ *
+ *  There are two ways to explicitly select the `STMImpl` instance:
+ *
+ *  1. set the JVM system property "scala.stm.impl" to the name of a class
+ *     that implements `STMImpl`; or
+ *
+ *  2. arrange for `STMImpl.select` or `STMImpl.trySelect` to be called
+ *     before any `Ref`s are constructed and before any atomic blocks are
+ *     executed.
+ *
+ *  Setting the JVM system property "scala.stm.impl" is equivalent to making a
+ *  call to `STMImpl.select(System.getProperty("scala.stm.impl"))` before any
+ *  other `STMImpl` selections.
+ *
+ *  If there is no explicitly selected `STMImpl` instance and the classpath
+ *  contains a class `scala.concurrent.stm.impl.DefaultFactory` that extends
+ *  `scala.concurrent.stm.impl.STMImpl.Factory`, then an instance of that
+ *  class will be instantiated and used to generate the `STMImpl` instance.
+ *  ScalaSTM implementations are encouraged to implement `DefaultFactory` so
+ *  that if a user includes the implementation's JAR file, it will be
+ *  automatically selected.
+ *
+ *  If no explicit selection has been made and there is no definition of
+ *  `scala.concurrent.stm.impl.DefaultFactory` present in the classpath, then
+ *  ScalaSTM will fall back to the reference implementation
+ *  "scala.concurrent.stm.ccstm.CCSTM".
+ *
+ *  @author Nathan Bronson
+ */
 object STMImpl {
   trait Factory {
     def createInstance(): STMImpl
@@ -33,10 +67,9 @@ object STMImpl {
 
   /** Installs `Class.forName(implClassName)` as the system-wide STM
    *  implementation if no `STMImpl` has yet been chosen, or verifies that
-   *  `implClassName` was previously selected.
-   *
-   *  @throws IllegalStateException if a different STM implementation has
-   *      already been selected
+   *  `implClassName` was previously selected, throwing 
+   *  `IllegalStateException` if a different STM implementation has already
+   *  been selected
    */
   def select(implClassName: String) {
     if (!trySelect(implClassName)) {
@@ -47,10 +80,8 @@ object STMImpl {
 
   /** Installs `impl` as the system-wide STM implementation if no `STMImpl`
    *  has yet been chosen, or verifies that `impl` is equal to the previously
-   *  selected instance.
-   *
-   *  @throws IllegalStateException if an STM implementation has already been
-   *      selected and `impl != instance`
+   *  selected instance, throwing `IllegalStateException` if an STM
+   *  implementation has already been selected and `impl != instance`
    */
   def select(impl: STMImpl) {
     explicitChoice = impl
@@ -87,33 +118,9 @@ object STMImpl {
 }
 
 /** `STMImpl` gathers all of the functionality required to plug an STM
- *  implementation into `scala.concurrent.stm`.  Only one implementation can
- *  be selected, because `Ref`s and atomic blocks from different STM
- *  implementations are not compatible.  `STMImpl.instance` returns the
- *  `STMImpl` instance that has been selected for this program execution.
- *
- *  There are two ways to explicitly select the `STMImpl` instance:
- *   1) set the JVM system property "scala.stm.impl" to the name of a class
- *      that implements `STMImpl`; or
- *   2) arrange for `STMImpl.select` or `STMImpl.trySelect` to be called
- *      before any `Ref`s are constructed and before any atomic blocks are
- *      executed.
- *  Setting the JVM system property "scala.stm.impl" is equivalent to making a
- *  call to `STMImpl.select(System.getProperty("scala.stm.impl"))` before any
- *  other `STMImpl` selections.
- *
- *  If there is no explicitly selected `STMImpl` instance and the classpath
- *  contains a class `scala.concurrent.stm.impl.DefaultFactory` that extends
- *  `scala.concurrent.stm.impl.STMImpl.Factory`, then an instance of that
- *  class will be instantiated and used to generate the `STMImpl` instance.
- *  ScalaSTM implementations are encouraged to implement `DefaultFactory` so
- *  that if a user includes the implementation's JAR file, it will be
- *  automatically selected.
- *
- *  If no explicit selection has been made and there is no definition of
- *  `scala.concurrent.stm.impl.DefaultFactory` present in the classpath, then
- *  ScalaSTM will fall back to the reference implementation
- *  "scala.concurrent.stm.ccstm.CCSTM".
+ *  implementation into `scala.concurrent.stm`.  See the `STMImpl` companion
+ *  object for information on controlling which `STMImpl` is selected at run
+ *  time.
  *
  *  @author Nathan Bronson
  */
