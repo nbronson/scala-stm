e5f3bfde5489518835a76c8a0abcb22088a4e988
Nathan Grasso Bronson <nbronson@cs.stanford.edu>
2012-10-14 12:47:45 -0700

    allow scala version to be specified when building docs

--- api/gendoc.sh
+++ api/gendoc.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # Takes a git branch as an argument, and updates that branch's ScalaDoc
-# subdirectory.  Grabs the version number from the project/build.properties in
+# subdirectory.  Second argument is the version of Scala under which to
+# generate the docs.  Version is taken from project/build.properties in
 # the specified branch.
 
 BRANCH="$1"
+SBT_SCALA_VER=""
+if [ -n "$2" ]; then
+  SBT_SCALA_VER="++$2"
+fi
 
 set -e   # exit on any command failure
 set -x   # print all of the commands before running them
@@ -17,7 +22,7 @@ mkdir $TMP
   git clone https://github.com/nbronson/scala-stm.git ;
   cd scala-stm ; \
   git checkout "$BRANCH" ; \
-  sbt doc )
+  sbt $SBT_SCALA_VER doc )
 
 VERSION=`awk -F'"' '/^version/ {print $2}' $TMP/scala-stm/build.sbt`
 if [ -x "$VERSION" ]; then
